59c8f5e245ab07763593cb2844af762bb0b81b11
codewitHK1 <kumarhk1002@gmail.com>
2024-02-23 09:07:23 +0530

    created express server and added user authentication processes

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import connectToMongoDB from "./db/connectDb.js";
+
+import authRoutes from "./routes/auth.routes.js";
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server Running on port ${PORT}`);
+});
